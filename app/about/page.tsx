a248512a61d6848726c37d959c6a3517cb4f807c
Kent Miguel <kentmiguel10@gmail.com>
2023-09-29 15:24:24 -0400

    Added github actions

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import Base from "../components/layouts/base";
 import ProfileHeader from "../components/pages/header";
 import ContainerLayout from "../components/layouts/container";
+import InProgressImage from "../../public/aboutme-option10.webp";
+import Image from "next/image";
 
 const page = () => {
   return (
     <Base>
       <ContainerLayout className="flex flex-col space-y-4">
-        <div>About Me</div>
+        <h1 className="text-3xl font-semibold tracking-tight text-gray-12 md:text-4xl">
+          Blog
+        </h1>
+        <div className="text-gray-11">Work in progress.</div>
+        <Image
+          className="rounded-2xl border border-gray-6"
+          src={InProgressImage}
+          alt="image"
+        />
       </ContainerLayout>
     </Base>
   );
