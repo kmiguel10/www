2a405f6590b951a5a5a0250d8cb0f2443da198cc
Kent Miguel <kentmiguel10@gmail.com>
2023-10-06 11:17:51 -0400

    tell next.js that route is intentionally dynamic by adding this

--- app/components/home/running.tsx
+++ app/components/home/running.tsx
@@ -1,6 +1,8 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export default async function Running() {
   const supabase = createServerComponentClient({ cookies });
   const { data: runs } = await supabase.from("running").select();
