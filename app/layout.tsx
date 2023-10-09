55c298144ff066b8b2024e4609f7d21406162760
Kent Miguel <kentmiguel10@gmail.com>
2023-10-09 14:30:29 -0400

    Fixed layout and deleted unused file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ variable: "--inter-font", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kent Miguel",
