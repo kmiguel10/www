cd23b16418368469caebac4fc1b9d69e2817d798
Kent Miguel <kentmiguel10@gmail.com>
2023-10-13 18:47:09 -0400

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import Base from "@components/layouts/base";
 import ProfileHeader from "@components/ui/header";
 import ContainerLayout from "@components/layouts/container";
-import InProgressImage from "../../public/aboutme-option3.webp";
-import Image from "next/image";
-import { ChevronLeft } from "lucide-react";
-import Button from "@components/ui/button/index";
 
 export const metadata = {
   title: "Projects",
@@ -14,25 +10,7 @@ export const metadata = {
 export default function AboutMe() {
   return (
     <Base pageSlug="/projects">
-      <ContainerLayout className="flex flex-col space-y-4">
-        <h1 className="text-3xl font-semibold tracking-tight text-gray-12 md:text-4xl">
-          Projects
-        </h1>
-        <div className="text-gray-11">Work in progress.</div>
-        <Image
-          className="rounded-2xl border border-gray-6"
-          src={InProgressImage}
-          alt="image"
-        />
-        <Button
-          variant="secondary"
-          intent="primary"
-          href="/"
-          leftIcon={<ChevronLeft />}
-        >
-          Return home
-        </Button>
-      </ContainerLayout>
+      <ContainerLayout className="flex flex-col space-y-4"></ContainerLayout>
     </Base>
   );
 }
