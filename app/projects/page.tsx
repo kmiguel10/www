b7033194ceadc287ed12667e9babbde4e2092caa
Kent Miguel <kentmiguel10@gmail.com>
2023-10-13 18:48:16 -0400

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,11 @@
 import React from "react";
 import Base from "@components/layouts/base";
 import ProfileHeader from "@components/ui/header";
-import ContainerLayout from "@components/layouts/container";
 
 export const metadata = {
   title: "Projects",
 };
 
 export default function AboutMe() {
-  return (
-    <Base pageSlug="/projects">
-      <ContainerLayout className="flex flex-col space-y-4"></ContainerLayout>
-    </Base>
-  );
+  return <Base pageSlug="/projects"></Base>;
 }
