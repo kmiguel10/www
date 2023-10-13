d519c97ff252e5735301868d72964d46a2f1d8bd
Kent Miguel <kentmiguel10@gmail.com>
2023-10-13 18:49:03 -0400

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,5 @@
-import React from "react";
-import Base from "@components/layouts/base";
-import ProfileHeader from "@components/ui/header";
-
 export const metadata = {
   title: "Projects",
 };
 
-export default function AboutMe() {
-  return <Base pageSlug="/projects"></Base>;
-}
+export default function AboutMe() {}
