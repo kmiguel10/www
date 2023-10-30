3784d489b6b13a4a2571c1d1e464a7da5d42b157
Kent Miguel <kentmiguel10@gmail.com>
2023-10-30 10:41:12 -0400

    Dark Mode (Default Theme) + God Rays Css on Header + Adjusted height of global css
    Dark mode is defaulted and commented out toggle with light mode until fix is found

--- app/components/ui/nav-bar/nav-bar-desktop.tsx
+++ app/components/ui/nav-bar/nav-bar-desktop.tsx
@@ -6,11 +6,11 @@ import clsx from "clsx";
 import { SunMoon } from "lucide-react";
 
 export default function NavbarDesktop({ selected }: NavBarProps) {
-  const toggleTheme = () => {
-    if (document.body.classList.contains("dark"))
-      document.body.classList.remove("dark");
-    else document.body.classList.add("dark");
-  };
+  // const toggleTheme = () => {
+  //   if (document.body.classList.contains("dark"))
+  //     document.body.classList.remove("dark");
+  //   else document.body.classList.add("dark");
+  // };
   return (
     <nav className="pointer-events-auto sticky top-0 z-popover hidden h-12 items-center border-b border-gray-6 bg-white px-4 dark:bg-black md:flex">
       {/* <Logo />  Can insert picure here or KM logo*/}
@@ -34,12 +34,12 @@ export default function NavbarDesktop({ selected }: NavBarProps) {
       })}
       <div className="flex-grow" />
       <div className="ml-2">
-        <button
+        {/* <button
           onClick={toggleTheme}
           className="px-3 py-1 border border-stone-200 rounded-full drop-shadow-sm text-sm text-stone-800 dark:text-white bg-white/40 dark:bg-black/40 backdrop-blur-lg hover:border-stone-300 transition-colors dark:border-stone-500 dark:hover:border-stone-400"
         >
           <SunMoon />
-        </button>
+        </button> */}
       </div>
     </nav>
   );
