26b42d7052d60f014968fe8d00e7e38dc5ada516
Kent Miguel <kentmiguel10@gmail.com>
2023-10-30 10:45:27 -0400

    Turned off mobile dark theme toggle

--- app/components/ui/nav-bar/nav-bar-mobile.tsx
+++ app/components/ui/nav-bar/nav-bar-mobile.tsx
@@ -9,11 +9,11 @@ import clsx from "clsx";
 import { SunMoon } from "lucide-react";
 
 export default function NavbarMobile({ selected }: NavBarProps) {
-  const toggleTheme = () => {
-    if (document.body.classList.contains("dark"))
-      document.body.classList.remove("dark");
-    else document.body.classList.add("dark");
-  };
+  // const toggleTheme = () => {
+  //   if (document.body.classList.contains("dark"))
+  //     document.body.classList.remove("dark");
+  //   else document.body.classList.add("dark");
+  // };
   return (
     <nav className="pointer-events-auto sticky top-0 z-popover flex h-12 items-center border-b border-gray-6 bg-white px-4 dark:bg-black md:hidden">
       {/* <Logo /> */}
@@ -37,12 +37,12 @@ export default function NavbarMobile({ selected }: NavBarProps) {
         );
       })}
       <div className="flex-grow" />
-      <button
+      {/* <button
         onClick={toggleTheme}
         className="px-3 py-1 border border-stone-200 rounded-full drop-shadow-sm text-sm text-stone-800 dark:text-white bg-white/40 dark:bg-black/40 backdrop-blur-lg hover:border-stone-300 transition-colors dark:border-stone-500 dark:hover:border-stone-400"
       >
         <SunMoon />
-      </button>
+      </button> */}
     </nav>
   );
 }
